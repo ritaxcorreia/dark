52281f1c771f72ed46f92299e9f3eaf994e6e65a
Rita <rita@ritacorreia.co>
2020-11-04 20:46:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ fetch(
 // .js-character is the div where the character data will be displayed
 
 fetch(
-	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/quote"
+	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/character"
 )
 	.then((response) => {
 		if (!response.ok) {
@@ -59,3 +59,7 @@ fetch(
 			quoteArea.innerHTML = `<h2>Quote:</h2><p><em> ${quote} </em></p><h2>Author: </h2><p>${author}</p><h2>Season:</h2><p>${season}</p><h2>Episode:</h2> <p>${episode}</p>`;
 		});
 	});
+
+{
+	/* <option value="character-name"></option>; */
+}
