010a95df57c2bc49ecaf7f1aa508bc1202e797a1
Rita <rita@ritacorreia.co>
2020-11-04 20:33:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 "use strict";
 
-const healthButton = document.querySelector("#getDataButton");
+const randomButton = document.querySelector(".random-button");
 
-const displayOl = document.querySelector(".health-data-list");
+const characterDropdownButton = document.querySelector(".dropdown");
+
+const quoteArea = document.querySelector(".js-quote");
+
+// .js-quote is the div where the quote will be displayed
 
 fetch(
-	"https://data.cityofnewyork.us/api/views/jb7j-dtam/rows.json?accessType=DOWNLOAD"
+	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/quote"
 )
 	.then((response) => {
 		if (!response.ok) {
-			const myStatusText = `Oops - ${response.statusText}`;
+			const myStatusText = `Oops - There was a ${response.statusText} error`;
 			throw Error(myStatusText);
 		}
 		return response.json();
@@ -17,13 +21,41 @@ fetch(
 	.then((jsonData) => {
 		console.log("jsonData", jsonData);
 
-		healthButton.addEventListener("click", function () {
-			jsonData.data.forEach((item) => {
-				const li = document.createElement("li");
-				const deaths = item[12];
-				const diseaseDescription = item[9];
-				li.innerText = `${deaths} - ${diseaseDescription}`;
-				displayOl.appendChild(li);
-			});
+		randomButton.addEventListener("click", function () {
+			const object =
+				jsonData[Math.floor(Math.random() * jsonData.length)];
+			const author = object.author;
+			const season = object.season;
+			const episode = object.episode;
+			const quote = object.quote;
+			quoteArea.classList.add("quote-class");
+			quoteArea.innerHTML = `<h2>Quote:</h2><p><em> ${quote} </em></p><h2>Author: </h2><p>${author}</p><h2>Season:</h2><p>${season}</p><h2>Episode:</h2> <p>${episode}</p>`;
+		});
+	});
+
+// .js-character is the div where the character data will be displayed
+
+fetch(
+	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/quote"
+)
+	.then((response) => {
+		if (!response.ok) {
+			const myStatusText = `Oops - There was a ${response.statusText} error`;
+			throw Error(myStatusText);
+		}
+		return response.json();
+	})
+	.then((jsonData) => {
+		console.log("jsonData", jsonData);
+
+		randomButton.addEventListener("click", function () {
+			const object =
+				jsonData[Math.floor(Math.random() * jsonData.length)];
+			const author = object.author;
+			const season = object.season;
+			const episode = object.episode;
+			const quote = object.quote;
+			quoteArea.classList.add("quote-class");
+			quoteArea.innerHTML = `<h2>Quote:</h2><p><em> ${quote} </em></p><h2>Author: </h2><p>${author}</p><h2>Season:</h2><p>${season}</p><h2>Episode:</h2> <p>${episode}</p>`;
 		});
 	});
