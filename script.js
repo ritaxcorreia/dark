d2b075e774cf333d71006db55be1d658ad42d63f
Rita <rita@ritacorreia.co>
2020-11-04 21:29:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const characterDropdownButton = document.querySelector(".dropdown");
 
 const quoteArea = document.querySelector(".js-quote");
 
+const characterArea = document.querySelector(".js-character");
+
+const selectElement = document.querySelector("#characters");
+
 // .js-quote is the div where the quote will be displayed
 fetch(
 	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/quote"
@@ -46,17 +50,22 @@ fetch(
 	.then((jsonData) => {
 		console.log("jsonData", jsonData);
 
-		characterDropdownButton.addEventListener("click", function () {
-			//populate dropdown with character list
+		//populate dropdown with character list
+		jsonData.forEach(function (object) {
+			const optionElement = document.createElement("option");
+			const name = object.name;
+			optionElement.appendChild(document.createTextNode(name));
+			selectElement.appendChild(optionElement);
+		});
 
-			const object =
-				jsonData[Math.floor(Math.random() * jsonData.length)];
-			const author = object.author;
-			const season = object.season;
-			const episode = object.episode;
-			const quote = object.quote;
-			quoteArea.classList.add("quote-class");
-			quoteArea.innerHTML = `<h2>Quote:</h2><p><em> ${quote} </em></p><h2>Author: </h2><p>${author}</p><h2>Season:</h2><p>${season}</p><h2>Episode:</h2> <p>${episode}</p>`;
+		characterDropdownButton.addEventListener("click", function () {
+			// const aliases = jsonData.aliases;
+			// const portrayedYoung = jsonData.portrayed.Young;
+			// const portrayedAdult = jsonData.portrayed.Adult;
+			// const portrayedOld = jsonData.portrayed.Old;
+			// const occupation = jsonData.occupation;
+			// characterArea.classList.add("quote-class");
+			// characterArea.innerHTML = `<h2>Name:</h2> ${name}<br><h2>Aliases:</h2> ${aliases}<br><h2>Portrayed by:</h2> ${portrayedYoung}, ${portrayedAdult}, ${portrayedOld}<br><h2>Occupation:</h2> ${ocupation}`;
 		});
 	});
 
