e85722fc3836f62b666cac6e87888d50266eb971
Rita <rita@ritacorreia.co>
2020-11-05 14:34:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,52 @@ const characterArea = document.querySelector(".js-character");
 
 const selectElement = document.querySelector("#characters");
 
+fetch(
+	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/character"
+)
+	.then((response) => {
+		if (!response.ok) {
+			const myStatusText = `Oops - There was a ${response.statusText} error`;
+			throw Error(myStatusText);
+		}
+		return response.json();
+	})
+	.then(function (data) {
+		console.log("data", data);
+		// store character list for later use
+		const characterData = data;
+
+		// Create an <option> for every breed
+		characterData.forEach(function (object) {
+			const optionElement = document.createElement("option");
+			const name = object.name;
+			optionElement.value = object.char_id;
+			optionElement.appendChild(document.createTextNode(name));
+			selectElement.appendChild(optionElement);
+		});
+
+		//  .js-character is the div where the character data will be displayed
+		selectElement.addEventListener("change", function (event) {
+			const characterChosen = event.target.value;
+			console.log(characterChosen);
+
+			const foundCharacter = data.find(
+				(element) => (element = characterChosen)
+			);
+
+			console.log(foundCharacter);
+
+			const name = foundCharacter.name;
+			const aliases = foundCharacter.aliases;
+			const portrayedYoung = foundCharacter.portrayed.Young;
+			const portrayedAdult = foundCharacter.portrayed.Adult;
+			const portrayedOld = foundCharacter.portrayed.Old;
+			const occupation = foundCharacter.occupation;
+			characterArea.classList.add("quote-class");
+			characterArea.innerHTML = `<h2>Name:</h2> ${name}<br><h2>Aliases:</h2> ${aliases}<br><h2>Portrayed by:</h2> ${portrayedYoung}, ${portrayedAdult}, ${portrayedOld}<br><h2>Occupation:</h2> ${occupation}`;
+		});
+	});
+
 // .js-quote is the div where the quote will be displayed
 fetch(
 	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/quote"
@@ -33,43 +79,5 @@ fetch(
 			const quote = object.quote;
 			quoteArea.classList.add("quote-class");
 			quoteArea.innerHTML = `<h2>Quote:</h2><p><em> ${quote} </em></p><h2>Author: </h2><p>${author}</p><h2>Season:</h2><p>${season}</p><h2>Episode:</h2> <p>${episode}</p>`;
-		});
-	});
-
-// .js-character is the div where the character data will be displayed
-fetch(
-	"https://cors-anywhere.herokuapp.com/https://dark-api.herokuapp.com/api/v1/character"
-)
-	.then((response) => {
-		if (!response.ok) {
-			const myStatusText = `Oops - There was a ${response.statusText} error`;
-			throw Error(myStatusText);
-		}
-		return response.json();
-	})
-	.then((jsonData) => {
-		console.log("jsonData", jsonData);
-
-		//populate dropdown with character list
-		jsonData.forEach(function (object) {
-			const optionElement = document.createElement("option");
-			const name = object.name;
-			optionElement.appendChild(document.createTextNode(name));
-			selectElement.appendChild(optionElement);
-		});
-
-		// when user selects option from dropdown menu
-		selectElement.addEventListener("change", function (characterChoice) {
-			const characterChosen = characterChoice.target.name;
-			console.log(characterChosen);
-
-			const name = jsonData.name;
-			const aliases = jsonData.aliases;
-			const portrayedYoung = jsonData.portrayed.Young;
-			const portrayedAdult = jsonData.portrayed.Adult;
-			const portrayedOld = jsonData.portrayed.Old;
-			const occupation = jsonData.occupation;
-			characterArea.classList.add("quote-class");
-			characterArea.innerHTML = `<h2>Name:</h2> ${name}<br><h2>Aliases:</h2> ${aliases}<br><h2>Portrayed by:</h2> ${portrayedYoung}, ${portrayedAdult}, ${portrayedOld}<br><h2>Occupation:</h2> ${occupation}`;
 		});
 	});
